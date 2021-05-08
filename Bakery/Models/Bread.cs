ef31c05f1428ab33f7a04da6c4d3873aa2289a2f
Michael Burton <micbur1@gmail.com>
2021-05-08 14:13:08 -0700

    add a bread constructor in bread.cs

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -2,7 +2,12 @@ namespace Bakery.Models
 {
   public class Bread
   {
+    public int NumberOfLoaves {get; set;}
 
+    public Bread(int numberOfLoaves)
+    {
+      NumberOfLoaves = numberOfLoaves;
+    }
     // properties, methods, etc. will go here.
 
   }
