a06184ef658e76a790dddb311901971ebd61eae3
Michael Burton <micbur1@gmail.com>
2021-05-08 17:25:42 -0700

    add functionallity for BreadTotal() to cpass test for calculatei the cost of ordering two or three loaves of bread

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -2,13 +2,19 @@ namespace Bakery.Models
 {
   public class Bread
   {
-    public int NumberOfLoaves {get; set;}
+    public int NumberOfOrderedLoaves {get; set;}
 
-    public Bread(int numberOfLoaves)
+    public Bread(int numberOfOrderedLoaves)
     {
-      NumberOfLoaves = numberOfLoaves;
+      NumberOfOrderedLoaves = numberOfOrderedLoaves;
     }
     // properties, methods, etc. will go here.
-
+    public int BreadTotal(int numberOfOrderedLoaves)
+    {
+      int numberOfFreeLoaves= numberOfOrderedLoaves/3;
+      int numberOfChargedLoaves = numberOfOrderedLoaves- numberOfFreeLoaves;
+      int BreadCost= numberOfChargedLoaves*5;
+      return BreadCost; 
+    }
   }
 }
