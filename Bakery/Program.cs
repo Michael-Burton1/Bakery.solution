745c45c57104aeadeb29ca2fcdb2e14d1af6d28b
Michael Burton <micbur1@gmail.com>
2021-05-08 18:57:55 -0700

    add Main() to program to sstart consoles writelines for the UI

---
+++ Bakery/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace TriangleDetector
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to Bon Pan the world renouned bakery of the stars!");
+      Console.WriteLine("__________________________________________________________________");
+      Console.WriteLine("We have a couple of specials that we are running today:");
+      Console.WriteLine("Bread: buy two loaves($5/each) get the thrird one free.");
+      Console.WriteLine("Pastries: 1 for $2 or 3 for $5");
+
+    }
+  }
+}
