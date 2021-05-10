e3f6dac3c7b5a4317c8e0840bd148aca305454e4
Michael Burton <micbur1@gmail.com>
2021-05-09 18:06:02 -0700

    WIP: fix errors in build to be able to access the Bread and Pastry classes within program

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -17,8 +17,8 @@ namespace Bakery.Models
       Console.WriteLine("Pastries: 1 for $2 or 3 for $5");
       Console.WriteLine("How many pastries would you like to order?");
       int numberOfOrderedPastries= int.Parse(Console.ReadLine());
-      Int costForBread = int.BreadCost(numberOfOrderedLoaves);
-      Int costForPastries = int.PastryCost(numberOfOrderedPastries);
+      int costForBread = int.BreadCost(numberOfOrderedLoaves);
+      int costForPastries = int.PastryCost(numberOfOrderedPastries);
       int tip= 250;
       int total=tip +costForBread+ costForPastries;
       Console.WriteLine("Thank you for your order.");
